1ccd01cc30bf106da06602ffb02a3fcc9e99cea3
Emerson Morgado <silvestrevan@gmail.com>
2020-02-20 08:50:38 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/br/com/emersonmorgado/mcuml/resources/CategoriaResources.java
+++ src/main/java/br/com/emersonmorgado/mcuml/resources/CategoriaResources.java
@@ -1,16 +1,26 @@
 package br.com.emersonmorgado.mcuml.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import br.com.emersonmorgado.mcuml.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest está funcionando";
+    public List listar(){
+
+        Categoria cat1 = new Categoria(1,"informática");
+        Categoria cat2 = new Categoria(2,"Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
