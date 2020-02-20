3cdcd444331dbc5bd2e5e0f8e4cf1eb5ef32432b
Emerson Morgado <silvestrevan@gmail.com>
2020-02-20 10:29:45 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/emersonmorgado/mcuml/McumlApplication.java
+++ src/main/java/br/com/emersonmorgado/mcuml/McumlApplication.java
@@ -1,13 +1,31 @@
 package br.com.emersonmorgado.mcuml;
 
+import br.com.emersonmorgado.mcuml.domain.Categoria;
+import br.com.emersonmorgado.mcuml.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class McumlApplication {
+public class McumlApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(McumlApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
