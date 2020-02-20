82f61a3b5b0e485a30c39db66030fc699fad3aab
Emerson Morgado <silvestrevan@gmail.com>
2020-02-20 09:13:36 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/br/com/emersonmorgado/mcuml/domain/Categoria.java
+++ src/main/java/br/com/emersonmorgado/mcuml/domain/Categoria.java
@@ -1,10 +1,17 @@
 package br.com.emersonmorgado.mcuml.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Geracao de chave primaria
     private Integer id;
     private String nome;
 
