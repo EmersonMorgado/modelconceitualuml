caa3bfba2dd3a55ebebf91a487df9d9807203b49
Emerson Morgado <silvestrevan@gmail.com>
2020-02-20 07:59:26 -0300

    Projeto criado

---
+++ src/main/java/br/com/emersonmorgado/mcuml/McumlApplication.java
@@ -0,0 +1,13 @@
+package br.com.emersonmorgado.mcuml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McumlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McumlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emersonmorgado/mcuml/McumlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emersonmorgado.mcuml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McumlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
