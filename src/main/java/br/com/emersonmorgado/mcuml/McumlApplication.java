3b064fb30856a649598ac65f34048f5fb657360f
Emerson Morgado <silvestrevan@gmail.com>
2020-02-24 14:57:33 -0300

    Produto e associacao muitos para muitos.

--- src/main/java/br/com/emersonmorgado/mcuml/McumlApplication.java
+++ src/main/java/br/com/emersonmorgado/mcuml/McumlApplication.java
@@ -1,12 +1,15 @@
 package br.com.emersonmorgado.mcuml;
 
 import br.com.emersonmorgado.mcuml.domain.Categoria;
+import br.com.emersonmorgado.mcuml.domain.Produto;
 import br.com.emersonmorgado.mcuml.repositories.CategoriaRepository;
+import br.com.emersonmorgado.mcuml.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -15,6 +18,8 @@ public class McumlApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McumlApplication.class, args);
@@ -26,6 +31,18 @@ public class McumlApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 
+		Produto p1 = new Produto(null, "Computador",2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 }
