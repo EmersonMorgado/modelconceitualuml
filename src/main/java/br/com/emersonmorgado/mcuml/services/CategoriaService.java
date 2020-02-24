5293aa1c27e5e2c311bb621ce1063e9fc42828ee
Emerson Morgado <silvestrevan@gmail.com>
2020-02-24 15:27:15 -0300

    Ajustes no endpoint / categorias/{id}

--- src/main/java/br/com/emersonmorgado/mcuml/services/CategoriaService.java
+++ src/main/java/br/com/emersonmorgado/mcuml/services/CategoriaService.java
@@ -2,6 +2,7 @@ package br.com.emersonmorgado.mcuml.services;
 
 import br.com.emersonmorgado.mcuml.domain.Categoria;
 import br.com.emersonmorgado.mcuml.repositories.CategoriaRepository;
+import br.com.emersonmorgado.mcuml.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ public class CategoriaService {
     public Categoria buscar(Integer id){
 
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
